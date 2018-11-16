997d15b60ae26a85b0ce3d49d41c31c5f673bef1
Elijah2005 <league@WTS22.attlocal.net>
2018-11-15 18:25:38 -0800

    Finished IPODSHUFFLE

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -1,29 +1,37 @@
 package _01_IntroToArrayLists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 //Copyright The League of Amazing Programmers, 2015
+public class _06_IPodShuffle implements ActionListener {
+	ArrayList<Song> s1 = new ArrayList<Song>();
+	JButton b = new JButton();
+	JFrame f = new JFrame();
+	JPanel p = new JPanel();
 
-public class _06_IPodShuffle {
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-		// Song s = new Song("demo.mp3");
+		Song koconut = new Song("koconut.mp3");
+		Song bro = new Song("Everyday Bro.mp3");
+		Song duck = new Song("The Duck Song.mp3");
 		// s.play();
 		// s.setDuration(5);
-		ArrayList<Song> s1 = new ArrayList<Song>();
-
-		JButton j = new JButton();
-		j.setText("Surprise Me!");
-		JFrame f = new JFrame();
-		JPanel p = new JPanel();
 		f.add(p);
-		p.add(j);
+		p.add(b);
+		b.setText("Surprise Me!");
 		f.setVisible(true);
 		f.pack();
+		b.addActionListener((ActionListener) this);
+		s1.add(koconut);
+		s1.add(bro);
+		s1.add(duck);
 
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
@@ -37,5 +45,14 @@ public class _06_IPodShuffle {
 
 	public static void main(String[] args) {
 		new _06_IPodShuffle();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		int randomSong = new Random().nextInt(s1.size());
+		Song s = s1.get(randomSong);
+		s.play();
+
 	}
 }
