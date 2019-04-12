b85ae9ff1c1d5f9f86f562a948a6a60f4ded5ed8
Elijah2005 <league@WTS22.attlocal.net>
2019-04-11 18:28:56 -0700

    almost done

--- src/_05_String_Integer_Pair/StringIntegerPair.java
+++ src/_05_String_Integer_Pair/StringIntegerPair.java
@@ -1,16 +1,15 @@
 package _05_String_Integer_Pair;
 
-import java.util.ArrayList;
-
 public class StringIntegerPair {
 	// 1. create a private array of Strings called keys. Don't initialize it.
 	private String[] keys;
 	// 2. create a private array of integers called values.
 	private int[] values;
 
-	StringIntegerPair(){
-		//3. initialize both member arrays to a length of 0
-		keys
+	StringIntegerPair() {
+		// 3. initialize both member arrays to a length of 0
+		keys = new String[0];
+		values = new int[0];
 	}
 
 	// 4. Complete the steps in the put method
@@ -18,20 +17,35 @@ public class StringIntegerPair {
 		// A. iterate through the keys. If you find a key that matches
 		// the passed in String, set the value at that location to the
 		// passed in value and return from the method.
+		for (int i = 0; i < keys.length; i++) {
+			if (keys[i].equals(key)) {
+				keys[i] = key;
+			}
+		}
 
 		// B. create a String array that is one element longer than the keys
-
+		String[] moreKeys = new String[keys.length + 1];
 		// C. create an integer array that is one element longer than values
-
+		int[] moreValues = new int[values.length + 1];
 		// D. set the last element of the new String array to the passed in key
-
+		moreKeys[moreKeys.length - 1] = key;
 		// E. set the last element of the new int array to the passed in value
-
+		moreValues[moreValues.length - 1] = value;
 		// F. iterate through the keys and values and copy the elements to the new
 		// arrays
+		for (int i = 0; i < keys.length; i++) {
+			moreKeys[i] += keys[i];
+		}
+		for (int i = 0; i < values.length; i++) {
+			moreValues[i] += values[i];
+		}
 
 		// G. Set the keys and values arrays equal to the new arrays created in steps B
 		// and C.
+		for (int i = 0; i < moreValues.length; i++) {
+			keys[i] = moreKeys[i];
+			values[i] = moreValues[i];
+		}
 	}
 
 	// 5. Complete the method so it returns the value located at the passed in key.
