1fb860dda558fb58b4e8fb36fa5b3ed82313fc11
League Student <league@WTS9.attlocal.net>
2019-05-30 18:28:14 -0700

    not done

--- src/Checkpoint.java
+++ src/Checkpoint.java
@@ -5,11 +5,24 @@ public class Checkpoint {
 public static void main(String[] args) {
 ArrayList<Cow> hungryCows = new ArrayList<Cow>();
 int numberOfCows = new Random().nextInt();
-hungryCows.a
+for (int i = 0; i < numberOfCows; i++) {
+	Cow random = new Cow();
+	hungryCows.add(random);
+	
+}
+for (int i = 0; i < hungryCows.size(); i++) {
+	Cow cow = new Cow();
+	cow.feed();
+	
+	
+}
 }
 }
 class Cow {
-    public void feed(){}
+    public void feed(){
+    	System.out.println("fed");
+    }
   }
+
 
 
